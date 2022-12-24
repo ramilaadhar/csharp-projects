4ed29440f865be7cb1dd73a31b5ad94add241e9a
unknown <rami.laadhar@gmail.com>
2022-12-24 16:34:25 +0100

    First commit

---
+++ magic_number/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static int ask_number(int min,int max)
+        {
+            int user_number = min -1;
+            while ((user_number < min) || (user_number > max))
+            {
+                Console.Write("Give a number betwen " + min + " and " + max + ": ");
+                string answer = Console.ReadLine();
+                try
+                {
+                    user_number = int.Parse(answer);
+                    if ((user_number < min) || (user_number > max))
+                    {
+                        Console.WriteLine("Error,the number must be betwen " + min + "and " + max + ":");
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("Error, enter a valid number");
+                }
+            
+            }
+            return user_number;
+        }
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            const int nbrmin = 1;
+            const int nbrmax = 10;
+            int magic_nb = rand.Next(nbrmin, nbrmax + 1);
+            
+               int number = magic_nb + 1;
+               // int nblife = 4;
+             // while  (nblife > 0)
+             for (int nblife = 4; nblife >0; nblife--) 
+              {
+                Console.WriteLine();
+                Console.WriteLine("You have " + nblife + " chance");
+                number = ask_number(nbrmin, nbrmax);
+                    
+               //Console.WriteLine("guess a number betwean " + nbrmin + "and" + nbrmax);
+               //string guess_nbrs = Console.ReadLine();
+               //guess_nbr = int.Parse(guess_nbrs);
+                 if (magic_nb < number)
+                 {
+                  Console.WriteLine("this number is more big then the magic number");
+                 }
+                 else if (magic_nb > number)
+                 {
+                 Console.WriteLine("this number is more less then the magic number");                  
+                 }
+                 else
+                 {
+                    Console.WriteLine("Good Job ! Your number is: " + magic_nb);
+                    break;
+                 }
+               // nblife--;
+
+              }
+              if (magic_nb != number)
+              { 
+                Console.WriteLine("You lose! the Magic Number is " + magic_nb);
+              }
+
+        }
+
+      
+    }
+}
---
+++ mathe_game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Schema;
+
+namespace mathe_game
+{
+    class program
+    {
+        static bool PoseQuestion (int Min, int Max)
+        {
+            Random rand = new Random();
+         int answerint = 0;
+            int result = 0;
+         while (true) 
+            {
+                int a = rand.Next(Min, Max + 1);
+                int b = rand.Next(Min, Max + 1);
+               Console.Write(a + " + " + b + "= ");
+                string answer = Console.ReadLine();
+                try
+                {
+                    answerint = int.Parse(answer);
+                    result = a + b;
+                    if (result == answerint)
+                    {
+                       return  true ;
+                    }
+                    return false ;
+                }
+                catch 
+                {
+                    Console.WriteLine("Erreur : you should give a number");
+                }
+              
+            } // aswerint
+        }
+        static void Main(string[] args) 
+        {
+            const int Nb_Min = 1;
+            const int Nb_Max = 10;
+            const int Nb_Question = 3;
+            int score = 0;  
+            for (int i = 0;i < Nb_Question;i++)
+            {
+                Console.WriteLine("question number " + (i +1) + "/" + Nb_Question);    
+                bool goodAnswer = PoseQuestion(Nb_Min, Nb_Max);
+                if (goodAnswer)
+                {
+                    Console.WriteLine("good Answer");
+                    score ++;
+                }
+                else
+                {
+                    Console.WriteLine("false Answer");
+                }
+            }
+            Console.WriteLine("Your score is " + score + "/" + Nb_Question);
+            
+        
+        }
+    }
+}
